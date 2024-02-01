98313696a531b28b09550acfc4d0768e3cd58712
Josephblaq <Josephforgod11@gmail.com>
2024-02-01 13:04:07 -0800

    _putchar.c updated

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
-
 /**
  * _putchar - writes a character to stdout
  * @c: the character to print
  *
- * Return: On success, 1. On error, -1 is returned, and errno is set appropriately.
+ * Return: On success, 1. On error, -1 is returned,
+ * and errno is set appropriately.
  */
 int _putchar(char c)
 {
